86e2d1fa69c40d9732a6ee3c13416a8b39de22f3
peter anyaogu <anyaogupeter601@gmail.com>
2022-09-10 12:16:49 +0100

    third book shop

---
+++ packages/contract/tests/ThirdBookShop/index.tsx
@@ -0,0 +1,17 @@
+import chai from "chai";
+
+describe("thirdbook shop", () => {
+  beforeEach(async () => {
+    // Todo
+  });
+  describe("when publishing book", () => {
+    it("does something", async () => {
+      // Todo
+    });
+  });
+  describe("when purchasing book", () => {
+    it("does something", async () => {
+      // Todo
+    });
+  });
+});
