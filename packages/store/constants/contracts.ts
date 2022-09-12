ae9c9cf34c30b846bb514929f5cde52ab3bede9b
peter anyaogu <anyaogupeter601@gmail.com>
2022-09-12 08:20:43 +0100

    contract integrations

---
+++ packages/store/constants/contracts.ts
@@ -0,0 +1,16 @@
+const contracts = {
+  USDContract: {
+    address: "",
+    abi: [],
+  },
+  BookContract: {
+    address: "",
+    abi: [],
+  },
+  ShopContract: {
+    address: "",
+    abi: [],
+  },
+};
+
+export default contracts;
